7a458ae5456c50db65a90a4e48189f3d4d196a11
pelukasnice <satakanaska@gmail.com>
2023-07-17 12:09:30 -0300

    proyecto funcional

--- app.js
+++ app.js
@@ -1,28 +1,83 @@
-const generarPDF = () => {
-  const legajo = document.getElementById('legajo').value;
-  const expediente = document.getElementById('expediente').value;
-  const oficinaFiscal = document.getElementById('oficinaFiscal').value;
-  const nombreSolicitante = document.getElementById('nombreSolicitante').value;
-  const dniTipo = document.getElementById('dniTipo').value;
-  const dni = document.getElementById('dni').value;
-  const domicilio = document.getElementById('domicilio').value;
-  const parentesco = document.getElementById('parentesco').value;
-  const nombreCadaver = document.getElementById('nombreCadaver').value;
-  const nombreCrematorio = document.getElementById('nombreCrematorio').value;
-  const provincia = document.getElementById('provincia').value;
-  const perito = document.getElementById('perito').value;
-  const nombreCocheria = document.getElementById('nombreCocheria').value;
+function validarFormulario() {
+  const legajo = document.querySelector('#legajo').value;
+  const expediente = document.querySelector('#expediente').value;
+  const oficinaFiscal = document.querySelector('#oficinaFiscal').value;
+  const nombreSolicitante = document.querySelector('#nombreSolicitante').value;
+  const dniTipo = document.querySelector('#dniTipo').value;
+  const dni = document.querySelector('#dni').value;
+  const domicilio = document.querySelector('#domicilio').value;
+  const parentesco = document.querySelector('#parentesco').value;
+  const nombreCadaver = document.querySelector('#nombreCadaver').value;
+  const nombreCrematorio = document.querySelector('#nombreCrematorio').value;
+  const provincia = document.querySelector('#provincia').value;
+  const perito = document.querySelector('#perito').value;
+  const nombreCocheria = document.querySelector('#nombreCocheria').value;
 
-  var fechaActual = new Date();
-  var dia = fechaActual.getDate();
-  var mes = fechaActual.toLocaleString('default', { month: 'long' });
-  var año = fechaActual.getFullYear();
-  
+  // Validar que todos los campos estén completos
+  if (
+    legajo === '' ||
+    expediente === '' ||
+    oficinaFiscal === '' ||
+    nombreSolicitante === '' ||
+    dniTipo === '' ||
+    dni === '' ||
+    domicilio === '' ||
+    parentesco === '' ||
+    nombreCadaver === '' ||
+    nombreCrematorio === '' ||
+    provincia === '' ||
+    perito === '' ||
+    nombreCocheria === ''
+  ) {
+    alert('Por favor, complete todos los campos antes de generar el PDF.');
+    return;
+  }
+
+  generarPDF(
+    legajo,
+    expediente,
+    oficinaFiscal,
+    nombreSolicitante,
+    dniTipo,
+    dni,
+    domicilio,
+    parentesco,
+    nombreCadaver,
+    nombreCrematorio,
+    provincia,
+    perito,
+    nombreCocheria
+  );
+}
+
+const generarPDF = (
+  legajo,
+  expediente,
+  oficinaFiscal,
+  nombreSolicitante,
+  dniTipo,
+  dni,
+  domicilio,
+  parentesco,
+  nombreCadaver,
+  nombreCrematorio,
+  provincia,
+  perito,
+  nombreCocheria
+) => {
+  // Obtener la fecha actual formateada
+  const getFormattedDate = (date) => {
+    const dia = date.getDate();
+    const mes = date.toLocaleString('default', { month: 'long' });
+    const año = date.getFullYear();
+    return ` el día ${dia} de ${mes} del año ${año}`;
+  };
+
+  const fechaActual = new Date();
+  const fechaEscrita = getFormattedDate(fechaActual);
+
   const horaActual = fechaActual.getHours();
   const minutosActual = fechaActual.getMinutes();
-
-
-  var fechaEscrita = " el día " + dia + " de " + mes + " del  año " + año;
 
   const doc = new jsPDF({
     format: 'legal'
@@ -30,38 +85,82 @@ const generarPDF = () => {
 
   doc.setFontSize(16);
 
-  // Contenido de la nota de solicitud de cremación
-  const margenDerecho = doc.internal.pageSize.width - 10; // Margen derecho
-  const margenInferior = doc.internal.pageSize.height - 10; // Margen inferior
+  const margenDerecho = doc.internal.pageSize.width - 10;
+  const margenInferior = doc.internal.pageSize.height - 10;
 
-  const contenido = `
-    LEGAJO N°: L-${legajo}
-    EXPTE. N°: ${expediente}
-    TRIBUNAL: ${oficinaFiscal}
+  // Generar contenido del PDF
+  const generateContent = () => {
+    const contenido = `
+      LEGAJO N°: L-${legajo}
+      EXPTE. N°: ${expediente}
+      TRIBUNAL: ${oficinaFiscal}
 
-                                    ACTA SOLICITUD DE CREMACION
+                                ACTA SOLICITUD DE CREMACION
 
-    En Mendoza,${fechaEscrita} a las ${horaActual}:${minutosActual} hs, se presenta el/la señor/ra ${nombreSolicitante} con documento ${dniTipo} N° ${dni} domiciliado en ${domicilio}, en su carácter de ${parentesco} a los fines de informar a Ud. mi intención de proceder a la cremación del cadáver de ${nombreCadaver} legajo N° ${legajo} en el crematorio ${nombreCrematorio} de la provincia de Mendoza. Certificado firmado por el Dr/a. ${perito}. El perito refiere que no necesita el cadáver para estudios posteriores.  El traslado lo efectuará la cochería ${nombreCocheria}.
-    
-               
-    
-    
-    FIRMA SOLICITANTE                                                 FIRMA PERITO
-    
-        
-  `;
+      En Mendoza,${fechaEscrita} a las ${horaActual}:${minutosActual} hs, se presenta el/la señor/ra ${nombreSolicitante} con documento ${dniTipo} N° ${dni} domiciliado en ${domicilio}, en su carácter de ${parentesco} a los fines de informar a Ud. mi intención de proceder a la cremación del cadáver de ${nombreCadaver} legajo N° ${legajo} en el crematorio ${nombreCrematorio} de la provincia de Mendoza. Certificado firmado por el Dr/a. ${perito}. El perito refiere que no necesita el cadáver para estudios posteriores.  El traslado lo efectuará la cochería ${nombreCocheria}.
 
-  const lines = doc.splitTextToSize(contenido, margenDerecho - 10); // Restar espacio para margen izquierdo
 
-  const lineHeight = 10; // Altura de línea
-  const x = 10; // Coordenada x para alineación izquierda
-  let y = 10; // Coordenada y inicial para la primera línea
-  
+
+                  SOLICITANTE                                                  PERITO
+          (Firma, aclaracion y dni)
+
+    `;
+
+    return doc.splitTextToSize(contenido, margenDerecho - 10);
+  };
+
+  const lines = generateContent();
+  const lineHeight = 10;
+  const x = 10;
+  let y = 60;
+
   lines.forEach(line => {
     doc.text(line, x, y);
-    y += lineHeight; // Aumentar la coordenada y para la siguiente línea
+    y += lineHeight;
   });
 
-  // Guardar el PDF
-  doc.save('solicitud_cremacion.pdf');
+  // Cargar la imagen
+ const logoImg = new Image();
+ logoImg.src = 'logo ministeriopublico.png'; 
+
+ // Esperar a que se cargue la imagen
+ logoImg.onload = function() {
+ // Dimensiones y posición de la imagen
+ const logoWidthCM = 70; // Ancho del logotipo en centímetros
+ const logoHeightCM = 40; // Alto del logotipo en centímetros
+
+ // Conversión de centímetros a puntos
+ const cmToPoints = 1; // Relación de conversión de centímetros a puntos
+ const logoWidth = logoWidthCM * cmToPoints; // Ancho del logotipo en puntos
+ const logoHeight = logoHeightCM * cmToPoints; // Alto del logotipo en puntos
+
+ // Dimensiones y posición de la imagen
+ const logoX = (doc.internal.pageSize.width - logoWidth) / 2; // Centrar horizontalmente
+ const logoY = 20; // Posición vertical en la página
+
+ // Agregar la imagen al documento
+ doc.addImage(logoImg, 'PNG', logoX, logoY, logoWidth, logoHeight);
+
+    // Guardar el PDF con el nombre del cadáver
+    const fileName = `${nombreCadaver}_cremacion.pdf`;
+    doc.save(fileName);
+  };
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
